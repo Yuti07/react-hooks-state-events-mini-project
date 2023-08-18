c6de941ed0525f23c0e10b95874766f4c81b2430
Yuti Thakkar <thakkaryuti@gmail.com>
2023-08-18 15:30:50 -0400

    adding mini project

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskList from "./TaskList";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+import Task from "./Task";
+import { TASKS, CATEGORIES } from "../data";
+import { v4 as uuidv4 } from "uuid";
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [tasks, setTasks] = useState(TASKS);
 
-function App() {
+  const handleCategoryFilter = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleTaskFormSubmit = (newTask) => {
+    const taskWithId = { ...newTask, id: uuidv4() };
+    setTasks([...tasks, taskWithId]);
+  };
+
+  const handleTaskDelete = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  const filteredTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
+
   return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+    <div className="app">
+      <h1>Task Manager</h1>
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onCategoryFilter={handleCategoryFilter}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        onTaskFormSubmit={handleTaskFormSubmit}
+      />
+      <TaskList tasks={filteredTasks} onDelete={handleTaskDelete} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,25 @@
 import React from "react";
+import Task from "./Task";
+import { v4 as uuidv4 } from "uuid";
 
-function TaskList() {
+const TaskList = ({ tasks, onDelete }) => {
+  const handleTaskDelete = (taskId) => {
+    onDelete(taskId);
+  };
+
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
+    <div className="task-list">
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          id={task.id}
+          text={task.text}
+          category={task.category}
+          onDelete={handleTaskDelete}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default TaskList;
